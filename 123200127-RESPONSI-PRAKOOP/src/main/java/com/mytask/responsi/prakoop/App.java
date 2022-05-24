8de48728bd8ff653c16d36e6be9ae5a74935a0ba
FarhanFansuri <96122019+FarhanFansuri@users.noreply.github.com>
2022-05-24 12:37:13 +0700

    Add files via upload

---
+++ 123200127-RESPONSI-PRAKOOP/src/main/java/com/mytask/responsi/prakoop/App.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mytask.responsi.prakoop;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class App {
+
+    public static void main(String[] args) throws Exception {
+       MovieView mv = new MovieView(); 
+        
+    }
+}
